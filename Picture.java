7a1cf306288d8f283a52bd7745cf08a362b65a06
BrandonM42 <g00320832@stu.raritanval.edu>
2025-01-27 12:15:46 -0500

    a
    a

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        sun2 = new Circle();
         drawn = false;
     }
 
@@ -57,6 +59,11 @@ public class Picture
             sun.changeSize(80);
             sun.makeVisible();
             drawn = true;
+            
+            sun2.changeColor("magenta");
+            sun2.moveHorizontal(200);
+            sun2.moveVertical(-79);
+            sun2.makeVisible();
         }
     }
 
